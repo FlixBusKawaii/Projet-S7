0b173eb5f2c7762e8987258002aa14b0e5175556
FlixBusKawaii <119581606+FlixBusKawaii@users.noreply.github.com>
2024-02-15 09:44:48 +0100

    Modifying style, updating main grid and added downloadable document

--- public/JS/main_mainpage.js
+++ public/JS/main_mainpage.js
@@ -1,5 +1,5 @@
 const URL_STAGE = 'http://localhost:3010/stage'
-const table = document.querySelector("#infostage");
+const grid = document.querySelector("#grid");
 
 
 const param = new URLSearchParams(document.location.search);
@@ -9,26 +9,60 @@ const User="token="+param.get("token")+"&role="+param.get("role");
 const get_stage = () => {
     fetch((URL_STAGE+"?key=stageDetail&"+User+"&Id_Stage="+param.get("id")))
     .then(res => res.json())
-    .then(json => constructTableProduct(json));
+    .then(json => constructGrid(json));
 };
 
-const constructTableProduct = item => {
-    table.innerHTML = "";
-    
-        let row = document.createElement("tr");
-        row.id = "?-" + item.id;
-        row.innerHTML = `
-        <td>${item.id}</td>
-        <td>${item.Nom}</td>
-        <td>${item.Statut}</td>
-        <td>${item.Stagiaire}</td>
-        <td>${item.Tuteur}</td>
-        <td>${item.Jury}</td>
-        <td>${item.Rapport}</td>
-        <td>${item.Eval}</td>
-        `;
-        table.appendChild(row); 
-    
+const constructGrid = item => {
+    let content = document.createElement("p");
+
+    let name = document.querySelector("#Nom");
+    name.innerHTML = "";
+    content.innerText = `${item.Name}`;
+    name.appendChild(content);
+    content = document.createElement("p");
+
+    let details = document.querySelector("#Detail");
+    details.innerHTML = "";
+    content.innerText = `${item.Detail}`;
+    details.appendChild(content);
+    content = document.createElement("p");
+
+    let state = document.querySelector("#Statut");
+    state.innerHTML = "";
+    content.innerText = `${item.State}`;
+    state.appendChild(content);
+    content = document.createElement("p");
+
+    let student = document.querySelector("#Stagiaire");
+    student.innerHTML = "";
+    content.innerText = `${item.Student}`;
+    student.appendChild(content);
+    content = document.createElement("p");
+
+    let tutor = document.querySelector("#Tuteur");
+    tutor.innerHTML = "";
+    content.innerText = `${item.Tutor}`;
+    tutor.appendChild(content);
+    content = document.createElement("p");
+
+    let teachers = document.querySelector("#Jury");
+    teachers.innerHTML = "";
+    content.innerText = `${item.Teacher}`;
+    teachers.appendChild(content);
+    content = document.createElement("p");
+
+    let doc = document.querySelector("#CR");
+    doc.innerHTML = "";
+    content.innerHTML = `<a href=file/${item._id}/${item.CR}>${item.CR}</a>`;
+    doc.appendChild(content);
+    content = document.createElement("p");
+
+    let grade = document.querySelector("#Note");
+    grade.innerHTML = "";
+    if(item.Note === undefined) content.innerText = "Pas de note";
+    else content.innerText = `${item.Note}`;
+    grade.appendChild(content);
+    content = document.createElement("p");
 };
 
 const send_doc = async data => fetch(URL_STAGE, {
